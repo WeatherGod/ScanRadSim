83f1a6a7607379936c6f1c225c79df3b2515b8d5
Ben Root <ben.v.root@gmail.com>
2011-03-17 16:52:26 -0500

    Significant rework of ScanRadSim.  Almost there to a working prototype!

--- lib/ScanRadSim/ScanSim.py
+++ lib/ScanRadSim/ScanSim.py
@@ -2,6 +2,7 @@ from BRadar.io import LoadLevel2
 #from RadarInterpolator import interp_radar
 from task import Task
 import numpy as np
+from datetime import timedelta
 
 
 class Simulator(object) :
@@ -18,31 +19,59 @@ class Simulator(object) :
         self.currTime = self.currItem['scan_time']
         self.currView = np.empty_like(self.currItem['vals'])
         self.currView.fill(np.nan)
+
+        # Make a special view of the data for internal purposes.
+        # the point of this is to organize this data as (radials, range_gate).
+        self._internalView = self.currView.view()
+        self._intervalView.shape = (-1, self.currView.shape[-1])
+
+        self._internalnext = self.nextItem['vals'].view()
+        self._internalnext.shape = (-1, self.nextItem['vals'].shape[-1])
+
+        self._internalcurr = self.currItem['vals'].view()
+        self._internalcurr.shape = (-1, self.currItem['vals'].shape[-1])
+
         self._set_slope()
 
 
     def _set_slope(self) :
-        self._slope = ((self.nextItem['vals'] - self.currItem['vals']) /
-                       float((self.nextItem['scan_time'] -
-                              self.currItem['scan_time']).microseconds))
+        self._slope = ((self._internalnext - self._internalcurr) /
+                       self._time_diff(self.currItem['scan_time'],
+                                       self.nextItem['scan_time']))
 
-    def update(self, timeElapsed, taskRadials) :
-        # Don't do += because we don't want to modify the original
-        # 'scan_time' value in the first self.currItem
-        self.currTime = self.currTime + timeElapsed
+    def _time_diff(self, time1, time2) :
+        """
+        Return the time difference in units of seconds,
+        including the microsecond portion.
+        """
+        timediff = time2 - time1
+        return timediff.seconds + (timediff.microseconds * 1e-6)
+
+    def update(self, theTask) :
+        timeElapsed = theTask.T
+        self.currTime += timeElapsed
         if self.currTime >= self.nextItem['scan_time'] :
             # We move onto the next file.
             self.currItem = self.nextItem
-            self.nextItem = self.radData.next()
+            self._internalcurr = self._internalnext
 
-            if self.nextItem is None :
-                return
-
+            try :
+                self.nextItem = self.radData.next()
+                self._internalnext = self.nextItem['vals'].view()
+                self._internalnext.shape = (-1, self.nextItem['vals'].shape[-1])
+            except StopIteration :
+                return False
+            
             self._set_slope()
 
-        self.currView[taskRadials, :] = (self._slope[taskRadials, :] *
-                                         (self.currTime - self.currItem['scan_time']).microseconds)
+        taskRadials = theTask.next()
+        #print type(taskRadials)
+        self._internalView[taskRadials] = ((self._slope[taskRadials] *
+                                            self._time_diff(self.currItem['scan_time'],
+                                                            self.currTime)) +
+                                           self._internalcurr[taskRadials])
 
+        return True
 
 
 from scipy.ndimage.measurements import find_objects, label
@@ -56,14 +85,27 @@ class SimpleSensingSys(object) :
     def __call__(self, radData) :
         tasksToRemove = self.prevTasks
 
-        labels, cnt = label(radData >= 35.0)
+        # The label and find_objects will slice only the
+        # relevant radials, but we still need something to
+        # represent the entire length of the radial.
+        fullRange = slice(None, None, None)
+
+        # Find the maximum value along each radial.
+        maxView = np.nanmax(radData, axis=-1)
+        labels, cnt = label(maxView >= 35.0)
         objects = find_objects(labels)
-        radialCnts = [(np.mgrid[anObject[:2]]).size // 2 for
-                      anObject in objects]
-        tasksToAdd = [Task(datetime.timedelta(seconds=2),
-                           datetime.timedelta(microseconds=10 * radCnt),
-                           anObject[:2]) for
-                      anObject, radCnt in zip(objects, radialCnts)]
+
+        allRadials = [radials + (fullRange,) for radials in objects]
+        radCnts = [np.prod([aSlice.stop - aSlice.start for
+                            aSlice in radials]) for
+                   radials in objects]
+        
+
+        tasksToAdd = [Task(timedelta(seconds=20),
+                           timedelta(milliseconds=10*cnt),
+                           radials, prt=10000) for
+                      radials, cnt in zip(allRadials, radCnts) if
+                      cnt >= 20]
         
         self.prevTasks = tasksToAdd
         return tasksToAdd, tasksToRemove
--- lib/ScanRadSim/task.py
+++ lib/ScanRadSim/task.py
@@ -1,7 +1,86 @@
-
+from itertools import cycle
+import datetime
+import numpy as np
 
 class Task(object) :
-    def __init__(self, updatePeriod, timeToComplete, name) :
+    def __init__(self, updatePeriod, timeFragment, radials, dwellTime=None, prt=None) :
+        """
+        updatePeriod, timeFragment must be timedeltas from datetime module.
+        dwellTime and prt must be ints in units of microseconds.
+
+        radials will be any iterator that returns an item that can be used to
+        access a part or sector of a numpy array upon a call to next()
+        """
         self.U = updatePeriod
-        self.T = timeToComplete
-        self.name = name
+        self.T = timeFragment
+
+        # Just keep doing these radials over and over...
+        self.radials = cycle(radials)
+
+        if dwellTime is None :
+            dwellTime = timeFragment * len(radials)
+        if prt is None :
+            # For now, just assume ten samples per dwell
+            prt = timeFragment / (10 * len(radials))
+
+        self.dwellTime = dwellTime
+        self.prt = prt
+
+    def __iter__(self) :
+        return self
+
+    def next(self) :
+        return self.radials.next()
+
+
+class Surveillance(Task) :
+    def __init__(self, timeFragment, dwellTime, gridshape, prt=None) :
+        """
+        timeFragment must be a timedelta object from the datetime module.
+        dwellTime and prt must be an int in units of microseconds.
+        gridshape must be a tuple of ints representing the shape of the
+            numpy array representing the radar grid.
+        """
+        scanVol = [slice(0, shape, None) for shape in gridshape]
+        radialCnt = np.prod(gridshape[:-1])
+        updatePeriod = datetime.timedelta(microseconds=dwellTime * radialCnt)
+        
+        # How many radials can we process within a time fragment?
+        chunkLen = int(((timeFragment.seconds * 1e6) + timeFragment.microseconds) // dwellTime)
+
+        if prt is None :
+            # For now, just assume ten samples per dwell...
+            prt = int(timeFragment.seconds * 1e6 + timeFragment.microseconds) // (10 * radialCnt)
+
+        Task.__init__(self, updatePeriod, timeFragment,
+                            ChunkIter(scanVol, chunkLen, radialCnt),
+                            dwellTime, prt)
+
+
+
+class ChunkIter(object) :
+    """
+    Takes a scanVolume, and wraps it in a manner that
+    would make a call to this object's next() function return
+    slices that would represent the next chunk of radials.
+
+    In other words, for a given scanVol and chunksize, produce
+    an iterator that returns a slice that represents a chunk of
+    scanVol on each call to next()
+    """
+    def __init__(self, scanVol, chunksize) :
+        self._radIndex = 0
+        self._start = scanVol[0].start
+        self._stop = scanVol[0].stop
+        self.chunksize = chunksize
+
+    def __iter__(self) :
+        return self
+
+    def next(self) :
+        start = self._radIndex
+        stop = (self._radIndex + self.chunksize) % (self._stop - self._start)
+        self._radIndex = stop
+        return (slice(start + self._start, stop + self._start, None), slice(None, None, None))
+
+
