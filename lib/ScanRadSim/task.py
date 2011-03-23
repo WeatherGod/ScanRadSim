1b4be81eb31b97e355c525c698d26b58898abebd
Ben Root <ben.v.root@gmail.com>
2011-03-23 16:49:12 -0500

    Massive changes to task.py.  In particular, the addition of a new VCP ScanJob class.  This still need additional testing.

--- lib/ScanRadSim/task.py
+++ lib/ScanRadSim/task.py
@@ -2,7 +2,7 @@ from itertools import cycle
 import datetime
 import numpy as np
 
-from NDIter import SliceIter
+from NDIter import SliceIter, BaseNDIter
 
 class ScanOperation(object) :
     def __init__(self, radSlice, tx_time, rx_time, wait_time=None) :
@@ -26,15 +26,8 @@ class ScanOperation(object) :
 
 
 class ScanJob(object) :
-    def __init__(self, updatePeriod, radials, dwellTime, prt=None, doCycle=False) :
+    def __init__(self, radials, doCycle=False) :
         """
-        updatePeriod must be a timedelta from the datetime module.
-            It represents the period at which the radials are iterated over
-
-        dwellTime and prt are also timedelta objects
-            dwellTime represents how much time it should take to process
-            each radial from a call to radials.next()
-
         radials is any iterator that returns an object that
             can be used to access a part of a numpy array upon
             a call to next().
@@ -42,32 +35,33 @@ class ScanJob(object) :
         doCycle will indicate whether or not to cycle through
         the radials.  Default is False.
         """
-        self.U = updatePeriod
         self.is_running = False
+        # This is so I can still access the info in radials,
+        # regardless of whether or not it gets wrapped by
+        # a cycle object.
+        self._origradials = radials
         self.radials = cycle(radials) if doCycle else radials
-        self.dwellTime = dwellTime
-        self.T = None
-
-        if prt is None :
-            # For now, assume 10 samples per dwell
-            prt = dwellTime / 10
-
-        self.prt = prt
-
         self.currslice = None
 
     def __iter__(self) :
         return self
 
     def _slicesize(self) :
-        return int(np.prod([len(range(aSlice.start, aSlice.stop, aSlice.step)) for
-                            aSlice in self.currslice[:-1]]))
+        if self.currslice is not None :
+            #print self.currslice
+            return int(np.prod([len(range(aSlice.start, aSlice.stop)) for
+                                aSlice in self.currslice[:-1]]))
+        else :
+            return 0
+
+    def _timeToComplete(self) :
+        return self.dwellTime * self._slicesize()
 
     def next(self) :
         # TODO: Assume a 10% duty cycle for now...
+        #print self, self._origradials, self._origradials._chunkIndices, self._origradials._chunkCnts
         self.currslice = self.radials.next()
-        #print "In task.next():", self.currslice
-        self.T = self.dwellTime * self._slicesize()
+        self.T = self._timeToComplete()
         txTime = self.T / 10
         rxTime = self.T - txTime
 
@@ -95,11 +89,187 @@ class StaticJob(ScanJob) :
         if updatePeriod < timeToComplete :
             updatePeriod = timeToComplete
 
-        ScanJob.__init__(self, updatePeriod, radials, dwellTime, prt, doCycle)
+        if prt is None :
+            # For now, assume 10 pulses
+            prt = dwellTime / 10
+
+        ScanJob.__init__(self, radials, doCycle)
+        self.U = updatePeriod
+        self.prt = prt
+        self.dwellTime = dwellTime
+        self.T = datetime.timedelta()
+        
+
+WSR_88D_PRT =  {1:  datetime.timedelta(microseconds=int(round(1e6 / 322))),
+                2:  datetime.timedelta(microseconds=int(round(1e6 / 446))),
+                3:  datetime.timedelta(microseconds=int(round(1e6 / 644))),
+                4:  datetime.timedelta(microseconds=int(round(1e6 / 857))),
+                5:  datetime.timedelta(microseconds=int(round(1e6 / 1014))),
+                6:  datetime.timedelta(microseconds=int(round(1e6 / 1095))),
+                7:  datetime.timedelta(microseconds=int(round(1e6 / 1181))),
+                8:  datetime.timedelta(microseconds=int(round(1e6 / 1282)))}
+
+WSR_88D_Cuts = {21 : (0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8),
+                12 : (0, 0, 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13),
+                11 : (0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13),
+               121 : (0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8),
+                31 : (0, 0, 1, 1, 2, 2, 3, 4),
+                32 : (0, 0, 1, 1, 2, 3, 4)}
+
+WSR_88D_Elevs = {21 : (0.5, 1.45, 2.4, 3.35, 4.3, 6.0, 9.0, 14.6, 19.5),
+                 12 : (0.5, 0.9, 1.3, 1.8, 2.4, 3.1, 4.0, 5.1, 6.4, 8.0, 10.0, 12.5, 15.6, 19.5),
+                 11 : (0.5, 1.45, 2.4, 3.35, 4.3, 5.25, 6.2, 7.5, 8.7, 10.0, 12.0, 14.0, 16.7, 19.5),
+                121 : (0.5, 1.45, 2.4, 3.35, 4.3, 6.0, 9.9, 14.6, 19.5),
+                 31 : (0.5, 1.5, 2.5, 3.5, 4.5),
+                 32 : (0.5, 1.5, 2.5, 3.5, 4.5)}
+
+WSR_88D_PRT_Num = {21 : (1, 5, 1, 5, (2, 5), (2, 5), (2, 5), (3, 5), 7, 7, 7),
+                   12 : (1, 5, 1, 5, 1, 5, (1, 5), (2, 5), (2, 5), (2, 5), (3, 5), (3, 5), 6, 7, 8, 8, 8),
+                   11 : (1, 5, 1, 5, (1, 5), (2, 5), (2, 5), (3, 5), (3, 5), 6, 7, 7, 7, 7, 7, 7),
+                  121 : (1, 8, 6, 4, 1, 8, 6, 4, (1, 8), 6, 4, (2, 8), 6, 4, (2, 4), 7, (3, 5), 7, 8, 8),
+                   31 : (1, 2, 1, 2, 1, 2, 2, 2),
+                   32 : (1, 5, 1, 5, (2, 5), (2, 5), (2, 5))}
+
+WSR_88D_PlsCnts = {21 : (28, 88, 28, 88, (8, 70), (8, 70), (8, 70), (12, 70), 82, 82, 82),
+                   12 : (15, 40, 15, 40, 15, 40, (3, 40), (3, 29), (3, 30), (3, 30), (3, 30), (3, 30), (3, 30), 38, 40, 44, 44, 44),
+                   11 : (17, 52, 16, 52, (6, 41), (6, 41), (6, 41), (10, 41), (10, 41), 43, 46, 46, 46, 46, 46, 46),
+                  121 : (11, 43, 40, 40, 11, 43, 40, 40, (6, 40), 40, 40, (6, 40), 40, 40, (6, 40), 40, 43, 43),
+                   31 : (63, 87, 63, 87, 63, 87, 87, 87),
+                   32 : (64, 220, 64, 220, (11, 220), (11, 220), (11, 220))}
+
+def _wsr_dwelltime(vcp) :
+    dwells = []
+    for index, cnt in zip(WSR_88D_PRT_Num[vcp],
+                          WSR_88D_PlsCnts[vcp]) :
+        tot = datetime.timedelta()
+        # Cast everything into tuples because of batch modes,
+        # which require summing in order to get a correct dwell time.
+        if not isinstance(index, tuple) :
+            index = (index,)
+            cnt = (cnt,)
+
+        for pulseCnt, indx in zip(cnt, index) :
+            tot += (pulseCnt * WSR_88D_PRT[indx])
+        dwells.append(tot)
+    return dwells
+
+def _wsr_prts(vcp) :
+    """
+    This isn't always *real* prts, just average prts in case of batch mode.
+    """
+    prts = []
+    for dwell, cnt in zip(_wsr_dwelltime(vcp),
+                          WSR_88D_PlsCnts[vcp]) :
+        if not isinstance(cnt, tuple) :
+            cnt = (cnt,)
+
+        prts.append(dwell / sum(cnt))
+    return prts
+
+class VCP(ScanJob) :
+    def __init__(self, vcp, gridshape, slices=None, elevOffset=0, updatePeriod=None, doCycle=True) :
+        """
+        A scan job that mimics the scanning pattern and timing
+        of a specified WSR-88D VCP.
+
+        If the entire grid is actually a raised slice, then I need
+           to know how much it is raised so that I can access the
+           proper information at that elevation level in the absolute
+           coordinate system.
+            Note, this has nothing to do with "slices" representing
+            a subset of the elevation angles. Only that the overall grid
+            that this job operates within is raised.
+
+        Update period is a timedelta object that specifies the update
+            period that this job should be set to.  Note that if it
+            is too small, it will be adjusted to the time it takes
+            to complete one run of the VCP.
+            If None, then use the default WSR-88D update time for
+            the time it takes to cover "gridshape".
+        """
+        if slices is None :
+            slices = [slice(0, shape, 1) for shape in gridshape]
+
+        chunkSize = 5
+
+        dwellTimes_elevs = _wsr_dwelltime(vcp)
+        prts_elevs = _wsr_dwelltime(vcp)
+
+        # This must be done before remaking gridshape because I
+        # need to know how wide the original grid was.
+        if updatePeriod is None :
+            updatePeriod = datetime.timedelta()
+            for aTime in dwellTimes_elevs :
+                updatePeriod += aTime * gridshape[1]
+
+        # These grid values are in the grid coordinate system
+        # (which is relative to the absolute coordinates by elevOffset),
+        # but has to be derived from the size of the data grid.
+        cutlist = range(*slices[0].indices(gridshape[0]))
+
+        # Remaking gridshape so that the slices and shapes agree.                
+        gridshape = [len(range(*aSlice.indices(shape))) for
+                     aSlice, shape in zip(slices, gridshape)]
+
+        # The elevation indices in slice coordinate system,
+        # (which is relative to the grid coordinate system by slice.start,
+        # (which, in turn, is relative to the abs coordinate system by elevOffset))
+        # in the order of execution.
+        sliceOffset = min(cutlist)
+        elevs, self._dwellTimes, self._prts = \
+                zip(*[(elev - elevOffset - sliceOffset, dwell, prt) for
+                      elev, dwell, prt in
+                      zip(WSR_88D_Cuts[vcp], dwellTimes_elevs, prts_elevs) if
+                      (elev - elevOffset in cutlist)])
+
+        #print slices[1], gridshape[1]
+        # The extra element is so that we can iterate all the way through.
+        azidivs = range(0, gridshape[1], chunkSize) + [gridshape[1]]
+
+        chunkIters = [cycle(slice(start, start + 1) for
+                            start in elevs),
+                      cycle(slice(start, stop, 1) for start, stop
+                            in zip(azidivs[:-1], azidivs[1:])),
+                      cycle((slice(0, gridshape[2], 1),))]
+        chunkCnts = [len(elevs), len(azidivs) - 1, 1]
+
+        #print self, "Azidivs:", azidivs
+        iterChunk = BaseNDIter(chunkIters, chunkCnts, (1, 0, 2))
+
+        timeToComplete = datetime.timedelta()
+        for elev_sliced in elevs :
+            timeToComplete += (dwellTimes_elevs[elev_sliced + elevOffset] * gridshape[1])
+
+        if timeToComplete > updatePeriod :
+            updatePeriod = timeToComplete
+
+        ScanJob.__init__(self, iterChunk, doCycle)
+        self.U = updatePeriod
+        self.T = datetime.timedelta()
+
+    def _get_dwelltime(self) :
+        # Based on the current elevation angle,
+        # return the dwell time.
+        if self.currslice is not None :
+            return self._dwellTimes[self._origradials._chunkIndices[0]]
+        else :
+            return datetime.timedelta()
+
+    dwellTime = property(_get_dwelltime, None, None, "The current dwell time")
+        
+    def _get_prt(self) :
+        # Based on the current elevation angle,
+        # return the prt
+        if self.currslice is not None :
+            return self._prts[self._origradials._chunkIndices[0]]
+        else :
+            return datetime.timedelta()
+
+    prt = property(_get_prt, None, None, "The current prt")
 
 
 class Surveillance(ScanJob) :
-    def __init__(self, dwellTime, gridshape, slices=None, prt=None) :
+    def __init__(self, dwellTime, gridshape, slices=None, prt=None, doCycle=True) :
         """
         dwellTime and prt must be integers in units of microseconds.
         gridshape must be a tuple of ints representing the shape of the
@@ -111,30 +281,71 @@ class Surveillance(ScanJob) :
         if slices is None :
             slices = [slice(0, shape, 1) for shape in gridshape]
 
+        chunkSize = 5
+
         gridshape = [len(range(*aSlice.indices(shape))) for
                      aSlice, shape in zip(slices, gridshape)]
 
         # Get the slice-chunking iterator for this task.
         #print "start:", [0] * len(gridshape), " stop:", gridshape
         iterChunk = SliceIter([0] * len(gridshape), gridshape,
-                              [1, 5, gridshape[-1]],
+                              [1, chunkSize, gridshape[-1]],
                               (1, 0, 2))
 
         radialCnt = int(np.prod(gridshape[:-1]))
-        dwellTime = datetime.timedelta(microseconds=dwellTime)# * radialCnt / len(iterChunk))
-        updatePeriod = dwellTime * radialCnt#len(iterChunk)
+        dwellTime = datetime.timedelta(microseconds=dwellTime)
+        updatePeriod = dwellTime * radialCnt
         #print "Dwell:", dwellTime, "  Radials:", radialCnt, "  ChunkCnt:", len(iterChunk)
 
-        ScanJob.__init__(self, updatePeriod, iterChunk,
-                               dwellTime, prt, doCycle=True)
+        ScanJob.__init__(self, iterChunk, doCycle)
+        self.U = updatePeriod
+        self.T = datetime.timedelta()
+        self.prt = prt
+        self.dwellTime = dwellTime
+
+
+
 
 if __name__ == '__main__' :
-    a = Surveillance(6400, (9, 366, 1000), slices=(slice(None), slice(0, 92, 1), slice(None)))
+    gridshape = (9, 366, 1000)
+    vol = (slice(1, 3), slice(0, 92, 1), slice(None))
 
-    print a.radials, len(a.radials)
+    print "Surveillance Job"
+    print "-----------------"
+    a = Surveillance(64000, gridshape, slices=vol, doCycle=False)
 
+    print a.radials, len(a.radials), a.U
     
-    for index in range(20) :
+    for index, b in zip(range(20), a) :
         #print a.radials._chunkIndices, a.radials._chunkCnts
-        b = a.next()
- 
+        print b.T, b.currslice
+
+    print "\n"
+    print "VCP 21 Job"
+    print "----------"
+    a = VCP(21, gridshape, slices=vol, doCycle=False, elevOffset=1)
+
+    print a.radials, len(a.radials), a.U
+
+    for index, b in zip(range(40), a) :
+        print b.T, b.currslice
+
+    print "\nVCP 21 Job"
+    print "----------"
+    a = VCP(21, gridshape, slices=vol, doCycle=False, elevOffset=1)
+
+    print a.radials, len(a.radials), a.U
+
+    for index, b in zip(range(40), a) :
+        print b.T, b.currslice
+
+    print "\nVCP 21 Job"
+    print "----------"
+    vol = (slice(1, 3), slice(92, 184, 1), slice(None))
+    a = VCP(21, gridshape, slices=vol, doCycle=False, elevOffset=0)
+
+    print a.radials, len(a.radials), a.U
+
+    for index, b in zip(range(40), a) :
+        print b.T, b.currslice
+
