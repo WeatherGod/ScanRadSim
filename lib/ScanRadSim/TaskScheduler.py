3f8d45ee3005d90e3a2e4c0314c1ddd5f278f892
Ben Root <ben.v.root@gmail.com>
2011-03-25 13:49:26 -0500

    Added _remain_time() to TaskScheduler to help correctly calculate the performance measures. Also added some robustness to those measures to reduce excess calls to other functions and proper edge-case handling.

--- lib/ScanRadSim/TaskScheduler.py
+++ lib/ScanRadSim/TaskScheduler.py
@@ -28,19 +28,61 @@ class TaskScheduler(object) :
         self.max_timeOver = timedelta()
         self.sum_timeOver = timedelta()
 
+    def _remain_time(self, job) :
+        """
+        This function is to return the remaining time for an
+        active task for a particular job.
+
+        This function has an important purpose:
+        If a job has a currently active task, then its loopcnt
+        will see that entire task as complete.  But that
+        completion includes things still to be done.
+
+        Let's illustrate this with an example.  Suppose we
+        query the scheduler every second, and at one point,
+        we set active a task that takes 5 seconds to complete.
+        For the next 4 queries, that job will have a loopcnt
+        that says that it has completed the entire task,
+        but that hasn't happened yet.
+
+        In such a situation, calculating the true_update_period()
+        will be wrong if the time given to it is only the time
+        "so far".  Therefore, we correct this by providing this
+        function which checks to see if the job has an active
+        task and returns the time remaining for the task.
+        """
+        if job is not None and job.is_running :
+            for index in range(len(self.active_tasks)) :
+                if job.currtask is self.active_tasks[index] :
+                    return job.currtask.T - self._active_time[index]
+
+            raise ValueError("Nothing matched!  Maybe this job's task belonged to another scheduler?")
+        else :
+            return timedelta(0)
+
     # NOTE: These next few functions are temporarially assuming the existance
     #       of a member variable called "self.surveil_job".
     def occupancy(self) :
-        return sum([(_to_usecs(aJob.T * len(aJob._origradials)) /
-                     float(_to_usecs(aJob.true_update_period(jobtime)))) for
-                    aJob, jobtime in zip(self.jobs + [self.surveil_job],
-                                         self._job_lifetimes + [self._schedlifetime]) if
-                    _to_usecs(aJob.T) != 0]) / self._concurrent_max
+        Ts, lens, Us = zip(*[(aJob.T, len(aJob._origradials),
+                              aJob.true_update_period(self._remain_time(aJob) + jobtime)) for
+                             aJob, jobtime in zip(self.jobs + [self.surveil_job],
+                                                  self._job_lifetimes + [self._schedlifetime])])
+        try :
+            return sum([(_to_secs(t * aLen) / float(_to_secs(u))) for
+                        t, aLen, u in zip(Ts, lens, Us) if
+                        t != timedelta() and u != timedelta.max]) / self._concurrent_max
+        except ValueError :
+            # In the case there are no valid values to sum
+            return np.nan
 
     def acquisition(self) :
-        U_times = [aJob.true_update_period(jobtime) for aJob, jobtime in
+
+        U_times = [aJob.true_update_period(self._remain_time(aJob) + jobtime) for
+                   aJob, jobtime in
                    zip(self.jobs + [self.surveil_job],
                        self._job_lifetimes + [self._schedlifetime])]
+        #print U_times
+        #print [str(life) for life in self._job_lifetimes + [self._schedlifetime]]
 
         try :
             max_u = _to_secs(max([prd for prd in U_times if prd != timedelta.max]))
@@ -59,9 +101,12 @@ class TaskScheduler(object) :
         In other words, the improvement factor is the average number of scans divided
         by the number of scans that would have been performed by a single radar beam.
         """
-        if len(self.jobs) > 0 :
-            return (sum([1.0 / _to_secs(aJob.true_update_period(joblife)) for
-                         aJob, joblife in zip(self.jobs, self._job_lifetimes)]) *
+        #print [self._remain_time(aJob) for aJob in self.jobs]
+        U_times = [aJob.true_update_period(self._remain_time(aJob) + joblife) for
+                   aJob, joblife in zip(self.jobs, self._job_lifetimes)]
+        if len(U_times) > 0 :
+            return (sum([1.0 / _to_secs(u) for u in
+                         U_times if u != timedelta.max]) *
                     _to_secs(base_update_period) / len(self.jobs))
         else :
             return np.nan
