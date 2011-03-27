7d716b6ba7aa7366efe578ea5ef33dfb9f1a7803
Ben Root <ben.v.root@gmail.com>
2011-03-27 17:25:19 -0500

    Added a new AdaptiveSensing system called "SimplePPI" which create tasks for each detected storm.

--- lib/ScanRadSim/AdaptSys.py
+++ lib/ScanRadSim/AdaptSys.py
@@ -92,3 +92,57 @@ class SimpleSensingSys(AdaptSenseSys) :
         return jobsToAdd, jobsToRemove
 register_sensing(SimpleSensingSys)
 
+
+
+class PPISensingSys(AdaptSenseSys) :
+    """
+    Just scan for every contiguous +35dBz region, but in a PPI fashion.
+    """
+    name = "SimplePPI"
+    def __init__(self, volume=None) :
+        self.prevJobs = []
+        AdaptSenseSys.__init__(self, volume)
+
+    def __call__(self, radData) :
+        jobsToRemove = self.prevJobs
+
+        # The label and find_objects will slice only the
+        # relevant radials, but we still need something to
+        # represent the entire length of the radial.
+        fullRange = slice(None, None, None)
+
+        labels, cnt = label(radData[self.volume] >= 35.0)
+
+        if cnt == 0 :
+            return [], jobsToRemove
+
+        objects = find_objects(labels)
+
+        allRadials = [radials[:-1] + (fullRange,) for radials in objects]
+        radCnts = [int(np.prod([aSlice.stop - aSlice.start for
+                                aSlice in radials[:-1]])) for
+                   radials in objects]
+        widths = [radials[1].stop - radials[1].start for
+                  radials in objects]
+
+        #print "Rad Counts:", radCnts
+        # Filter out the small storms
+        allRadials = [radials for radials, cnt in zip(allRadials, radCnts) if cnt >= 20]
+        widths = [width for width, cnt in zip(widths, radCnts) if cnt >= 20]
+        radCnts = [cnt for cnt in radCnts if cnt >= 20]
+
+        #totScanRads = sum(radCnts)
+        gridshape = radData[self.volume].shape
+        
+        jobsToAdd = [StaticJob(timedelta(seconds=40),
+                               #(radials,),
+                               ChunkIter(gridshape, width, radials),
+                               dwellTime=timedelta(microseconds=64000),
+                               prt=timedelta(microseconds=800)) for
+                     radials, cnt, width in zip(allRadials, radCnts, widths)]
+
+        self.prevJobs = jobsToAdd
+        return jobsToAdd, jobsToRemove
+register_sensing(PPISensingSys)
+
+
